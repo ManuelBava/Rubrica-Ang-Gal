f883bbaab52bd4ac42137ae46613ed26dbb2590e
ManuelBava <manuelbava1996@gmail.com>
2021-02-23 20:05:23 +0100

    implementati i component

--- rubrica-gal-client/src/app/app.module.ts
+++ rubrica-gal-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ErrorMessagePageComponent } from './error-message-page/error-message-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { CountPageComponent } from './count-page/count-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ErrorMessagePageComponent,
+    MainPageComponent,
+    SearchPageComponent,
+    CountPageComponent
   ],
   imports: [
     BrowserModule,
